4f5c2391f222a8a32cfabf4d9858ab0f7bf74708
warrior <spaceman.programmer@hotmail.com>
2024-05-10 00:02:53 +0200

    Revert "Sos calculator built"
    This reverts commit b455a268fa6991e05789d9046ecb6edd3feb0879.

--- Sos Calculator/src/main/java/calculator/interfaces/Speedup.java
+++
@@ -1,9 +0,0 @@
-package calculator.interfaces;
-
-public interface Speedup {
-    int getOneMinute();
-    int getFiveMinute();
-    int getOneHour();
-    int getThreeHour();
-    int getEightHour();
-}
